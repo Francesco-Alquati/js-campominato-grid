afbf5f2ad0bda317b4173a0077451a0c3c238ed0
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-07 18:29:06 +0200

    [UPD] Added style class "square" to the squares

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ function createSquare(){
     // CREO IL SINGOLO QUADRATO 
     let square = document.createElement('div');
 
+    // AGGIUNGO LA CLASSE SQUARE
+    square.classList.add('square');
+
     // RESTITUISCO IL QUADRATO
     return square;
 }
