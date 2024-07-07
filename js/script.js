a188874dcffdc60a18305bf3b1be97efe52886af
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-07 18:27:58 +0200

    [UPD] Created function to create square

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// FUNZIONE CHE CREA I QUADRATI DELLA GRIGLIA
+function createSquare(){
+
+    // CREO IL SINGOLO QUADRATO 
+    let square = document.createElement('div');
+
+    // RESTITUISCO IL QUADRATO
+    return square;
+}
